66256c7ee2a581755908b57afdb6914e667a4260
Sujeong Park <krystarline@gmail.com>
2022-06-04 14:43:16 +0900

    feat: Firebase 로그인 및 로그아웃 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,80 @@
+import { useState, useEffect, } from "react";
+import {
+  signInWithPopup,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  signOut,
+} from "firebase/auth";
+
+import { authService } from "./config/firebase";
+
 function App() {
-  return <h1>Hello, world!</h1>;
+  const [auth, setAuth] = useState(false || window.localStorage.getItem("auth") === true);
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    onAuthStateChanged(authService, async (user) => {
+
+      if (user) {
+        const token = await user.getIdToken();
+
+        window.localStorage.setItem("auth", true);
+        setToken(token);
+        setAuth(true);
+      }
+    });
+  }, []);
+
+  const loginWithGoogle = async () => {
+    const user = await signInWithPopup(authService, new GoogleAuthProvider());
+
+    if (user) {
+      window.localStorage.setItem("auth", true);
+      setAuth(true);
+    }
+  }
+
+  const loginWithGithub = async () => {
+    const user = await signInWithPopup(authService, new GithubAuthProvider());
+
+    if (user) {
+      window.localStorage.setItem("auth", true);
+      setAuth(true);
+    }
+  }
+
+  const logout = async () => {
+    try {
+      await signOut(authService);
+
+      setToken("");
+      setAuth(false);
+
+      window.location.reload();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      { auth ? (
+          <button onClick={logout}>
+            Logout
+          </button>
+        ) : (
+          <>
+            <button onClick={loginWithGoogle}>
+              Login with Google
+            </button><button onClick={loginWithGithub}>
+              Login with Github
+            </button>
+          </>
+        )
+      }
+    </>
+  );
 }
 
 export default App;
