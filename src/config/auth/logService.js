07eda394272fc4ac86a45a92436bd21642713377
younchong <ungun96@naver.com>
2022-06-04 19:42:17 +0900

    feat: login, logout firebase와 연결 및 /signup 서버로 전달

---
+++ src/config/auth/logService.js
@@ -0,0 +1,59 @@
+import { authService } from "./firebase";
+import {
+  signInWithPopup,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  signOut,
+} from "firebase/auth";
+import axios from "axios";
+
+export async function loginWithGoogle(setToken, setUser, setAuth) {
+  const loginResult = await signInWithPopup(
+    authService,
+    new GoogleAuthProvider()
+  );
+
+  if (loginResult) {
+    window.localStorage.setItem("auth", true);
+    setAuth(true);
+
+    const token = loginResult.user.accessToken;
+    const uid = loginResult.user.uid;
+    const result = await axios({
+      method: "post",
+      url: "http://localhost:8000/signup",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      data: {
+        uid,
+      },
+    });
+
+    setToken(token);
+    setUser(uid);
+
+    return result;
+  }
+}
+
+export async function loginWithGithub(setAuth) {
+  const user = await signInWithPopup(authService, new GithubAuthProvider());
+
+  if (user) {
+    window.localStorage.setItem("auth", true);
+    setAuth(true);
+  }
+}
+
+export async function logout(setToken, setUser, setAuth) {
+  try {
+    await signOut(authService);
+
+    setToken(null);
+    setUser(null);
+    setAuth(false);
+  } catch (error) {
+    console.error(error);
+  }
+}
