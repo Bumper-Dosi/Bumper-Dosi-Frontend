41eef1c76301b04b196a3a0f088ccaa68d905527
Sujeong Park <krystarline@gmail.com>
2022-06-07 14:00:34 +0900

    feat: 친구 추가 및 삭제 기능 구현 완료

--- src/components/FriendList/index.js
+++ src/components/FriendList/index.js
@@ -1,10 +1,9 @@
-// 친구목록 => 서버에 요청 (axios-get) / 접속한 user => token으로
 import axios from "axios";
 import React from "react";
 import { useState, useEffect } from "react";
 import styled from "styled-components";
 
-const LoginLayout = styled.div`
+const FriendListLayout = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -16,10 +15,10 @@ const LoginLayout = styled.div`
   right: 0;
 `;
 
-const LoginRow = styled.div`
+const FriendListRow = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
 
   position: absolute;
@@ -29,10 +28,32 @@ const LoginRow = styled.div`
 
   border-radius: 10px;
   background-color: rgba(0, 0, 0, 0.1);
+
+  nav {
+    position: fixed;
+    margin-right: 300px;
+  }
+
+  nav h1 {
+    position: flex;
+    margin-left: 30px;
+  }
+
+  nav button {
+    position: flex;
+    margin-left: 40px;
+  }
 `;
 
 function FriendList({ token }) {
   const [friendList, setFriendList] = useState([]);
+  const [friendName, setfriendName] = useState("");
+
+  const setFindFriendWithName = (event) => {
+    event.preventDefault();
+
+    setfriendName(event.target.value);
+  };
 
   useEffect(() => {
     if (token) {
@@ -42,7 +63,7 @@ function FriendList({ token }) {
 
   const getFriendList = async (token) => {
     try {
-      const res = await axios(
+      const res = await axios.get(
         "http://localhost:8000/friends",
         {
           headers: {
@@ -50,31 +71,96 @@ function FriendList({ token }) {
           },
         },
       );
-      setFriendList(res.data.friends); // console.log(res.data);
+      setFriendList(res.data.friends);
     } catch (error) {
       console.error(error);
     }
   };
 
-  return (
-    <LoginLayout>
-      <LoginRow>
-        <div>
-          <h1>FriendList</h1>
-          {
-            friendList.map((friend, index) => {
-              const { uid, name } = friend;
+  const addFriend = async (event) => {
+    event.preventDefault();
 
-              return (
-                <ul key={index} onClick={() => getFriendList(uid)}>
-                  <li>name: {name}</li>
-                </ul>
-              );
-            })
-          }
-        </div>
-      </LoginRow>
-    </LoginLayout>
+    try {
+      const res = await axios.post(
+        "http://localhost:8000/friends",
+        {
+          friendName,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        },
+      );
+      setFriendList(res.data.friends);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const deleteFriend = async (uid) => {
+    try {
+      const res = await axios.delete(
+        "http://localhost:8000/friends",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+          data: {
+            uid,
+          },
+        },
+      );
+      setFriendList(res.data.friends);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return ( // 아이콘 토글 만들기
+    <FriendListLayout>
+      <FriendListRow>
+        <form
+          onSubmit={addFriend}
+        >
+          <div>
+            <input
+              type="text"
+              value={friendName}
+              placeholder={"이름을 입력해주세요."}
+              onChange={setFindFriendWithName}
+            />
+          </div>
+        </form>
+        <nav>
+          <h1>FriendList</h1>
+          <ul>
+            {
+              friendList.map((friend, index) => {
+                const { uid, name } = friend;
+
+                return (
+                  <li
+                    id={uid}
+                    key={index}
+                  >
+                    name: {name}
+                    <button
+                      type="delete"
+                      onClick={() => deleteFriend(uid)}
+                    >
+                      삭제
+                    </button>
+                  </li>
+                );
+              })
+            }
+          </ul>
+        </nav>
+      </FriendListRow>
+    </FriendListLayout>
   );
 };
 
