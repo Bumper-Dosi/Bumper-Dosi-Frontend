fa65ea401f01fd526ab1db5278a2740734a29ed4
JinkwonHeo <hjinkwon912@gmail.com>
2022-06-08 21:26:17 +0900

    refacrot: css cursor 컨벤션에 따라 순서 변경

--- src/components/AlarmModal/index.js
+++ src/components/AlarmModal/index.js
@@ -15,16 +15,16 @@ const appear = keyframes`
   100% {
     opacity: 0;
   }
-`
+`;
 
 const DeleteButton = styled.button`
-  cursor: pointer;
   display: flex;
   border: none;
   background-color: transparent;
   font-weight: bold;
   font-size: 15px;
   color: red;
+  cursor: pointer;
 `;
 
 const AlarmModalLayout = styled.div`
@@ -37,10 +37,9 @@ const AlarmModalLayout = styled.div`
   padding: 10px 10px;
   font-family: "Arial Black", sans-serif;
   opacity: 0.8;
-  background-color: rgba(246,247,248, 0.8);
+  background-color: rgba(246, 247, 248, 0.8);
   box-shadow: 0px 5px 8px 3px rgb(0 0 0 / 30%),
-              0px 2px 5px -2px rgba(0, 0, 0, 0.418),
-              0px 2px 5px -7px rgb(0 0 0 / 20%);
+    0px 2px 5px -2px rgba(0, 0, 0, 0.418), 0px 2px 5px -7px rgb(0 0 0 / 20%);
   z-index: 4;
   transform: translate(-50%, -50%);
   animation: ${appear} 8s ease-in-out;
@@ -49,7 +48,7 @@ const AlarmModalLayout = styled.div`
 const AlarmModalContent = styled.div`
   text-align: center;
   padding: 10px;
-`
+`;
 
 function AlarmModal({ openModal, message }) {
   const [countdownTime, setCountdownTime] = useState(TIME.MODAL_DURATION_TIME);
@@ -57,7 +56,7 @@ function AlarmModal({ openModal, message }) {
 
   const handleCloseModal = () => {
     openModal(false);
-  }
+  };
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -77,12 +76,8 @@ function AlarmModal({ openModal, message }) {
 
   return (
     <AlarmModalLayout className="alarm-modal" onClick={handleCloseModal}>
-      <DeleteButton>
-        X
-      </DeleteButton>
-      <AlarmModalContent>
-        {message}
-      </AlarmModalContent>
+      <DeleteButton>X</DeleteButton>
+      <AlarmModalContent>{message}</AlarmModalContent>
     </AlarmModalLayout>
   );
 }
